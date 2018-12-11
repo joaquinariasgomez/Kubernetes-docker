e20726adcd3b8f1681d04e31bfbe82fafdcf18d8
empiremay <tatinarias@hotmail.com>
2018-12-11 18:13:09 +0100

    Changed Minikube version from 0.27.0 to 0.25.0

--- instalation.sh
+++ instalation.sh
@@ -2,12 +2,8 @@
 
 DOCKER_VERSION="18.03.0~ce-0~ubuntu"
 KUBERNETES_VERSION="1.10.1"
-MINIKUBE_VERSION="0.27.0"
-# Cannot upgrade helm to, i.e., 2.10.0 due to cluster servers imcompatibility
+MINIKUBE_VERSION="0.25.0"
 HELM_VERSION="2.8.2"
-ISTIO_VERSION="1.0.1"
-ERIC_ISTIO_VERSION="0.1.8"
-METRICS_SERVER_VERSION="v0.3.1"
 
 # Docker
 sudo apt-get update
@@ -25,18 +21,22 @@ curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v${KU
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v${MINIKUBE_VERSION}/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 sudo minikube start --vm-driver=none
 
-[ -d /root/.kube ] && sudo mv /root/.kube $HOME/.kube # this will write over any previous configuration
+[ -d /root/.kube ] && sudo mv /root/.kube $HOME/.kube
 sudo chown -R $USER $HOME/.kube
 sudo chgrp -R $USER $HOME/.kube
 
-[ -d /root/.minikube ] && sudo mv /root/.minikube $HOME/.minikube # this will write over any previous configuration
+[ -d /root/.minikube ] && sudo mv /root/.minikube $HOME/.minikube
 sudo chown -R $USER $HOME/.minikube
 sudo chgrp -R $USER $HOME/.minikube
 
 
 # helm
-sudo apt-get install socat
+sudo apt-get install -y socat
 curl -O https://kubernetes-helm.storage.googleapis.com/helm-v${HELM_VERSION}-linux-amd64.tar.gz
 tar xvzf helm-v${HELM_VERSION}-linux-amd64.tar.gz
 sudo mv linux-amd64/helm /usr/local/bin
 helm init --wait
+
+# pip3
+sudo apt-get update
+sudo apt-get install -y python3-pip
